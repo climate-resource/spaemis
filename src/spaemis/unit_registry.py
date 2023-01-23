5c757d5ccd489b777bd62ac925cf1032ae913242
Jared Lewis <jared@jared.kiwi.nz>
2023-01-23 12:10:41 +1100

    Add unit registry

---
+++ src/spaemis/unit_registry.py
@@ -0,0 +1,6 @@
+from openscm_units import unit_registry
+
+if not hasattr(unit_registry, "hydrogen"):
+    unit_registry.define("H = [hydrogen] = H2")
+    unit_registry.define("hydrogen = H")
+    unit_registry.define("t{symbol} = t * {symbol}".format(symbol="H"))
