66443bb59cd65bbeb2caa790ce612e03f37021d8
Jared Lewis <jared@jared.kiwi.nz>
2023-01-23 12:09:10 +1100

    Refactor gse_emis into a commandline command

---
+++ src/spaemis/commands/__init__.py
@@ -0,0 +1,2 @@
+from .gse_emis_command import run_gse_command
+from .base import cli
---
+++ src/spaemis/main.py
@@ -0,0 +1,6 @@
+from spaemis.commands import cli
+
+if __name__ == "__main__":
+    # Run the CLI tool
+    cli()
+
